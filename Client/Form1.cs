e33f1b21bf5e712cf84a486849cb63b7a271aa3d
Yushi_np28@top-academy.ru <Yushi_np28@top-academy.ru>
2024-06-04 21:14:02 +0300

    Добавьте файлы проекта.

---
+++ Client/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        public Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+        public static IPAddress ip = IPAddress.Parse("127.0.0.1");
+        public IPEndPoint ep = new IPEndPoint(ip, 1024);
+        string message = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+            Task.Run(Go);
+        }
+        private void Go()
+        {
+            try
+            {
+                s.Connect(ep);
+
+                if (s.Connected)
+                {
+                    while (true)
+                    {
+                        byte[] buffer = new byte[1024];
+                        int l = s.Receive(buffer);
+                        message = Encoding.UTF8.GetString(buffer, 0, l);
+                        //LabelTime.Text = message;
+                        Invoke(new Action(() => LabelTime.Text = message));
+                    }
+
+                }
+                else { s.Close(); }
+            }
+            catch { }
+
+        }
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            s.Close();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+
+    internal class Program
+    {
+
+        static async Task Main()
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ep = new IPEndPoint(ip, 1024);
+
+            s.Bind(ep);
+            s.Listen(10);
+
+            try
+            {
+
+                while (true)
+                {
+                    Socket sn = s.Accept();
+                    await Task.Run(() => { UpdateTime(sn); });
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Connect Error");
+            }
+            finally
+            {
+                s.Close();
+            }
+            async void UpdateTime(Socket socket)
+            {
+                // Socket socket = obj as Socket;
+                while (socket.Connected)
+                {
+                    try
+                    {
+                        socket.Send(Encoding.UTF8.GetBytes(DateTime.Now.ToLongTimeString()));
+                        await Task.Delay(1000);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Client closed");
+                    }
+                }
+            }
+        }
+
+    }
+}
+
